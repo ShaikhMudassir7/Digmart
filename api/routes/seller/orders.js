b30ae42067590ebc873ae31137746c645b4d6d13
Glen <dsouzaglen30@gmail.com>
2022-08-20 17:08:33 +0530

    Seller Dashboard

--- api/routes/seller/orders.js
+++ api/routes/seller/orders.js
@@ -3,15 +3,15 @@ const router = express.Router()
 
 const Orders = require("../../models/seller/orders");
 
-router.get('/', (req, res) => {
-    res.render("./seller/orders/orders")
+router.get('/new-orders', (req, res) => {
+    res.render("./seller/orders/newOrders")
 })
 
-router.get('/delivered', (req, res) => {
+router.get('/delivered-orders', (req, res) => {
     res.render("./seller/orders/delivered")
 })
 
-router.get('/shipment', (req, res) => {
+router.get('/shipment-orders', (req, res) => {
     res.render("./seller/orders/shipment")
 })
 module.exports = router
--- app.js
+++ app.js
@@ -34,7 +34,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.set('views', './views')
 app.set('view engine', 'ejs')
-app.use('/admin', adminRoute)
 app.use(express.static('public'))
 app.use('/css', express.static(__dirname + 'public/css'))
 app.use('/js', express.static(__dirname + 'public/js'))
