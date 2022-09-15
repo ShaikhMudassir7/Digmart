2a647c80166830a52297c3a1618a97ab76ecebb6
Hatim <hatmsb11@gmail.com>
2022-09-15 09:31:06 +0530

    user login final

--- api/routes/user/user.js
+++ api/routes/user/user.js
@@ -3,7 +3,6 @@ const router = express.Router()
 const mongoose = require('mongoose')
 
 router.get('/home',(req, res) => {
-    console.log(req.session.userid)
     res.render('./user/home',{user: req.session.userid})
 })
 
