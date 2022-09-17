167e88630d1f2f77899253e35c8307df1d63b4d3
Glen <dsouzaglen30@gmail.com>
2022-09-17 17:30:11 +0530

    user location

--- api/routes/user/user.js
+++ api/routes/user/user.js
@@ -1,13 +1,14 @@
 const express = require("express")
 const router = express.Router()
 const mongoose = require('mongoose')
+const https = require('https');
 
 const Category = require('../../models/admin/categorySchema');
 
-router.get('/',(req, res) => {
+router.get('/', (req, res) => {
     Category.find().exec()
         .then(docs => {
-            res.render('./user/home',{catData: docs, user: req.session.userid})
+            res.render('./user/home', { catData: docs, user: req.session.userid })
         })
         .catch(err => {
             console.log(err)
@@ -17,17 +18,45 @@ router.get('/',(req, res) => {
         })
 })
 
-router.get('/shop-by-category',(req, res) => {
-    Category.findOne({_id: req.query.id}).exec()
-    .then(docs => {
-        res.render('./user/shop-by-category',{catData: docs, user: req.session.userid})
-    })
-    .catch(err => {
-        console.log(err)
-        res.status(500).json({
-            error: err
+router.get('/shop-by-category', (req, res) => {
+    Category.findOne({ _id: req.query.id }).exec()
+        .then(docs => {
+            res.render('./user/shop-by-category', { catData: docs, user: req.session.userid })
         })
-    })
+        .catch(err => {
+            console.log(err)
+            res.status(500).json({
+                error: err
+            })
+        })
+})
+
+router.post('/api/pincode', (req, res) => {
+    var lat = req.body.lat
+    var long = req.body.long
+
+    https.get('https://api.opencagedata.com/geocode/v1/json?q=' + lat + '+' + long + '&key=' + process.env.OPENCAGE_API, response => {
+        let data = [];
+        console.log('Status Code:', response.statusCode);
+
+        response.on('data', chunk => {
+            data.push(chunk);
+        });
+
+        response.on('end', () => {
+            console.log('Response ended: ');
+            const val = JSON.parse(Buffer.concat(data).toString());
+            console.log(val.results[0].components.postcode)
+            res.json({
+                pincode: val.results[0].components.postcode
+            });
+        });
+    }).on('error', err => {
+        console.log('Error: ', err.message);
+        res.json({
+            pincode: 'error'
+        });
+    });
 })
 
 module.exports = router
--- public/js/user/home.js
+++ public/js/user/home.js
@@ -121,3 +121,47 @@ function wishlist(element) {
     }
 }
 
+function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition, showError);
+    } else {
+        alert("Geolocation is not supported by this browser.")
+    }
+}
+getLocation();
+
+function showPosition(position) {
+
+    $.ajax({
+        url: "/api/pincode",
+        type: "POST",
+        data: {
+            lat: position.coords.latitude,
+            long: position.coords.longitude,
+        },
+        dataType: 'json',
+        success: function (result) {
+           if(result.pincode != 'error'){
+            console.log(result.pincode)
+           }
+        }
+    });
+}
+
+function showError(error) {
+    switch (error.code) {
+        case error.PERMISSION_DENIED:
+            alert("User denied the request for Geolocation.")
+            // window.location.reload()
+            break;
+        case error.POSITION_UNAVAILABLE:
+            alert("Location information is unavailable.")
+            break;
+        case error.TIMEOUT:
+            alert("The request to get user location timed out.")
+            break;
+        case error.UNKNOWN_ERROR:
+            alert("An unknown error occurred.")
+            break;
+    }
+}
