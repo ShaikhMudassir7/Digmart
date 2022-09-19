c812cb2dcb3118bb3787c30af046adcbd2ec2f06
Hatim <hatmsb11@gmail.com>
2022-09-19 17:05:26 +0530

    product page

--- api/routes/user/product.js
+++ api/routes/user/product.js
@@ -12,10 +12,11 @@ router.get('/view-product/(:id)', (req, res) => {
     Products.findById(id,
         (err, element) => {
             if (!err) {
-                Variants.find({ 'prodID': id }).exec()
-                .then(docs => {
+                Variants.find({ prodID: id }).exec()
+                    .then(docs => {
+                        console.log(docs[0])
                         if (!err) {
-                            res.render('./user/product', { images: allImages, variantsData: docs, productData: element});
+                            res.render('./user/product', { images: allImages, variantData: docs[0], variantsData: docs, productData: element });
                         }
                     })
             } else {
@@ -24,6 +25,30 @@ router.get('/view-product/(:id)', (req, res) => {
 
         })
 })
+
+router.get('/variant/(:id)/(:colours)', (req, res) => {
+    const allImages = Variants.find().select("images")
+    var id = req.params.id;
+    var docsize
+    var vd;
+    Products.findById(id,
+        async (err, element) => {
+            if (!err) {
+                await Variants.find({ prodID: id })
+                    .then(docs => {
+                        vd = docs;
+                    })
+                await Variants.findOne({ prodID: id, colours: req.params.colours })
+                    .then(doc => {
+                        res.render('./user/product', { images: allImages, variantData: doc, variantsData: vd, productData: element });
+                    })
+            } else {
+                res.send('try-again')
+            }
+
+        })
+})
+
 
 
 module.exports = router
