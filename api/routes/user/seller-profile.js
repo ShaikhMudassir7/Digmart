e3d49027d00cdaf361d71ec05d59ed23a212cfa5
Hatim <hatmsb11@gmail.com>
2022-10-05 11:35:41 +0530

    seller gallery

--- api/routes/user/seller-profile.js
+++ api/routes/user/seller-profile.js
@@ -7,16 +7,23 @@ require("firebase/storage");
 
 const Seller = require("../../models/seller/seller");
 const SellerGall = require("../../models/seller/gallery");
+const Products = require('../../models/seller/product')
+const Variants = require('../../models/seller/variants')
 
 // Route of product page
 router.get("/(:sellerID)", async (req, res, next) => {
   var id = req.params.sellerID;
-  const images = await SellerGall.findOne({sellerID:id}).select("images");
-  
+  const images = await SellerGall.findOne({ sellerID: id }).select("images");
+  var varDocs = []
   const element = await Seller.findById(id);
-
-
-  SellerGall.find({sellerID:id})
+  var proDocs = await Products.find({ sellerID: id, status: 'Verified' })
+  for (let i = 0; i < proDocs.length; i++) {
+    if (proDocs[i].hasVariant) {
+      var doc = await Variants.find({ prodID: proDocs[i]._id })
+      varDocs.push(doc[0])
+    }
+  }
+  SellerGall.find({ sellerID: id })
     .select()
     .exec()
     .then((docs) => {
@@ -24,7 +31,7 @@ router.get("/(:sellerID)", async (req, res, next) => {
         galleryData: images,
         sellerID: id,
         sellerData: element,
-        user: req.session.userid,
+        user: req.session.userid, proDocs: proDocs, varDocs: varDocs
       });
     })
 });
