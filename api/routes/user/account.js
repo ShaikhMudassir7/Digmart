21f47468f5a8ed40b3eca33e3e7d9ea903a11f44
Hawaiza <hawaiza1234@gmail.com>
2022-11-01 20:42:38 +0530

    accounts

--- api/routes/user/account.js
+++ api/routes/user/account.js
@@ -10,4 +10,9 @@ router.get('/', async (req, res) => {
     res.render('./user/account', {user : req.session.userid})
 })
 
+router.get('/addresses', async (req, res) => {
+    var docs = await Address.find().select().exec()
+    res.render('./user/account-addresses', {addressData: docs, user : req.session.userid, noSearch: true })
+})
+
 module.exports = router
