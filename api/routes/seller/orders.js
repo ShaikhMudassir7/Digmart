eab0e258899ff16455deea65712473ed03740138
Asim Siddiqui <asimahmed.s@somaiya.edu>
2022-11-08 19:25:19 +0530

    Seller Orders Hawaiza

--- api/routes/seller/orders.js
+++ api/routes/seller/orders.js
@@ -2,11 +2,16 @@ const express = require("express")
 const router = express.Router()
 
 const Orders = require("../../models/user/order");
+const OrderItem = require('../../models/user/order_item');
 
 const checkAuth = require("../../middleware/seller/checkAuth")
 
-router.get('/new-orders', checkAuth, (req, res) => {
-    res.render("./seller/orders/newOrders", { sellerID: req.session.sellerID, pFname: req.session.pFname, pLname: req.session.pLname })
+router.get('/new-orders', checkAuth, async(req, res) => {
+    var docs = await Orders.find({ sellerID: req.session.sellerID }).select().exec()
+    // console.log(docs)
+    var doc = await OrderItem.find({ sellerID: req.session.sellerID }).select().populate('sellerID productID variantID').exec();
+     console.log(doc.length)
+    res.render("./seller/orders/newOrders", { newOrdersData: docs, itemDetails: doc, sellerID: req.session.sellerID, pFname: req.session.pFname, pLname: req.session.pLname })
 })
 
 router.get('/delivered-orders', checkAuth, (req, res) => {
