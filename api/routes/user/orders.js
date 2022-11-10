bd4a7050a6573f8000e5b5528e499a0769abdbd9
Hawaiza <hawaiza1234@gmail.com>
2022-11-10 14:00:29 +0530

    Orders

--- api/routes/user/orders.js
+++ api/routes/user/orders.js
@@ -2,10 +2,11 @@ const express = require("express")
 const router = express.Router()
 const mongoose = require('mongoose')
 
+const checkAuth = require("../../middleware/user/checkAuth")
 const Orders = require('../../models/user/order');
 const OrderItem = require('../../models/user/order_item');
 
-router.get('/', async (req, res) => {
+router.get('/', checkAuth, async (req, res) => {
     var docs = await OrderItem.find({ userID: req.session.userID }).sort({orderID : -1}).populate('sellerID productID variantID').exec()
     res.render('./user/orders', { orderItemsData: docs, user: req.session.userID, noSearch: true })
 })
